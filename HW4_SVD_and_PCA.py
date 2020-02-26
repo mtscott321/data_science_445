3f9a8305a6eb12b866db5ceb751f246bdbd6f459
mtscott321 <mtscott321@gmail.com>
2020-02-25 22:21:53 -0500

    DONE! any further commits are just making the code prettier and fixing the icky globals i defined when i was young and FOOLISH

--- HW4_SVD_and_PCA.py
+++ HW4_SVD_and_PCA.py
@@ -15,7 +15,7 @@ import numpy as np
 import math
 import random as rd
 from matplotlib import pyplot as plt, figure as fig
-
+from scipy import integrate
 
 #%%
 """
@@ -28,7 +28,7 @@ np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 x = []
 y = []
 m = 30
-d = 4
+d = 10
 A = np.zeros((m, d))
     
 #used to compute the X, Y arrays
@@ -47,23 +47,25 @@ def err():
     return sig * rd.randrange(-1, 1)
    
 #the function determining the actual values of the Y array
-def f(val):
-    return np.around(err() + math.sin(val), 2)
+def f(v):
+    return np.around(err() + math.sin(v), 2)
 
 #constructing the X and Y arrays based on the function
-def make_array():
+def make_array(l = m):
+    temp_a = np.zeros((l, d))
     prev = start
-    for i in range (0, m):
+    for i in range (0, l):
         prev = prev + dx
         x.append(np.around(prev, 2))
-        print(x)
         y.append(f(prev))
         for j in range(0, d):
-            A[i, j] = np.around(prev**j, 2)
+           # A[i, j] = np.around(prev**j, 2)
+            temp_a[i, j] = np.around(prev**j, 2) 
+    return temp_a
 
 #returns the x solution to Ax = b
-def least_sq():
-    U, S, Vt = SVD(A)
+def least_sq(mat = A):
+    U, S, Vt = SVD(mat)
     y_arr = np.array(y)
     c = np.zeros((1,d))
     
@@ -74,6 +76,7 @@ def least_sq():
         t = np.array(ut_b_over_sig)
         term = np.matrix.dot(t, Vt.T[:,i])
         c = c + term
+    del y_arr, U, S, Vt
     return c
 
 #based on the least squares result and the real y vals, returns the error
@@ -83,15 +86,27 @@ def least_sq_error_discrete(xvals, result):
         s = s + (f(xvals[i]) - result[i])**2
     return s
 
+def A_for_x(xval):
+    a_small = np.zeros((1, d))
+    for i in range(0,d):
+        a_small[0, i] = xval**d
+    return a_small
+
 #returns the error in the least squares approx for continuously defined functions
-def least_sq_error_cont(xvals, c):
-    
+def least_sq_error_cont(xvals, c, result):
+    def squid(w):
+        q = f(w) - np.matrix.dot(A_for_x(w), result)
+        return q**2
+    val_squared = integrate.quad(lambda w: squid(w), -1, 1)[0]
+    return pow(val_squared, 0.5)
 
 #multiplies three matrices together
 def multiply_three(A, B, C):
     BC = np.matrix.dot(B, C)
     ABC = np.matrix.dot(A, BC)
+    del BC
     return ABC
+
 
 #normalizes a matrix using the Euclidean/L2 norm
 def euclidean_normalized(vt):
@@ -115,6 +130,7 @@ def round_all_vals(mat, x=2):
 #calculates the singular value decomposition of matrix A
 def SVD(A):
     #define useful values
+    n = d
     min_mn = d
     if d > m:
         min_mn = m
@@ -163,6 +179,7 @@ def SVD(A):
 """
 Plotting x and y values and least square approx
 """
+"""
 plt.figure(figsize=(20, 10))
 
 x = []
@@ -173,21 +190,37 @@ plt.plot(x, result)
 plt.xlabel("X values")
 plt.ylabel("sin(x) in radians (red), least square fit (blue)")
 plt.plot(x, y, 'ro')
+del result
 plt.show
+"""
 
 #%%
 """
 Running the code several times for multiple m values and plotting the mean and std_dev
 """
-all_errors = []
-for i in range(0, 100):
-    #have to make x and y empty again
-    x = []
-    y = []
-    make_array()
-    result = np.dot(A, least_sq().T)       
-    all_errors.append(least_sq_error_cont(x, result)) 
 
-mean = np.average(all_errors)
-std = np.std(all_errors)
+all_errors = []
+ms = np.linspace(d+1, 100, 50, dtype= int)
+#i'm running out of variable names so hard right now that im going for animals
+means = []
+stds = []
+for lizard in ms:
+    m = lizard
+    for i in range(0, 40):
+        #have to make x and y empty again
+        x = []
+        y = []
+        A_new = make_array(lizard)
+        lst = least_sq(A_new).T
+        result = np.dot(A_new, lst)
+        all_errors.append(least_sq_error_cont(x, result, lst)) 
+
+    means.append(np.average(all_errors))
+    stds.append(np.std(all_errors))
+
+plt.figure(figsize = (20, 10))
+#plt.plot(ms, means, 'ro')
+plt.plot(ms, stds, 'go')
+plt.ylabel("Standard deviation in the error")
+plt.xlabel("m value")
 print("The mean value of the error for m = %d is: %.3f and the STD = %0.3f" %(m, mean, std))
