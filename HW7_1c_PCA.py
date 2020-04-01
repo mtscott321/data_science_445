f1894ddfcbcf087474beea3419ed2dfa949c40cd
mtscott321 <mtscott321@gmail.com>
2020-04-01 11:59:06 -0700

    changes

--- HW7_1c_PCA.py
+++ HW7_1c_PCA.py
@@ -12,52 +12,8 @@ from matplotlib import pyplot as plt, figure as fig
 
 #%%
 """
-
-#%%
-D = 30
-n = 400
-k = 2
-A = normal_stnd_dist_mat(D, n)
-Abar, emp_mean = dsf.center_mat(A)
-v = dsf.emp_variance(Abar, emp_mean)
-U, sig, Vt = dsf.SVD(A)
-projA = dsf.PCA(Abar, k)
-projAbar, proj_emp_mean = dsf.center_mat(projA)
-proj_v = dsf.emp_variance(projA, emp_mean)
-print(proj_v)
-sig_k_sq = sig[k,k] *sig[k,k]
-print(sig[k,k])
-print(sig_k_sq)
-
-#%%
-D = 20
-n = 40
-k = 4
-A = normal_stnd_dist_mat(D, n)
-U, sig, Vt = dsf.SVD(A)
-#center A
-Abar, emp_mean = dsf.center_mat(A)
-projA = dsf.PCA(Abar, k)
-
-newAbar,newemp_mean = dsf.center_mat(projA)
-v = dsf.emp_variance(newAbar, emp_mean)
-
-
-print(v)
-print(sig[k-1, k-1])
-print(sig[k-1, k-1] * sig[k-1, k-1])
-#%%
-print(A[:, 0]) #this prints the first column
-
-#%%
-A = dsf.normal_stnd_dist_mat(3,2)
-print(A)
-U, sig, Vt = dsf.SVD(A)
-print(dsf.multiply_three(U, sig, Vt))
+part 1d
 """
-
-#%%
-
 def error_with_changing_k(D, n, kstart, kend):
     A = dsf.normal_stnd_dist_mat(D, n)
     errors = []
@@ -79,6 +35,8 @@ plt.show()
 #%%
 
 """
+part 1c
+"""
 
 k = 3
 A = dsf.normal_stnd_dist_mat(50, 100)
@@ -88,7 +46,5 @@ var = dsf.emp_variance(A, k)
 sig_sq = sig[k-1,k-1]*sig[k-1, k-1]
 
 print(A.shape[1] * var)
-print(sig_sq)
 
-print(dsf.PCA_error(A, k))
-"""
+print(sig_sq)
